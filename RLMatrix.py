886f7ca07c4c5661727cb810f8897ad389671462
Soma Pal <78855113+soma345@users.noreply.github.com>
2021-04-26 21:16:37 -0500

    RL GridWorld Task Algo
    RL GridWorld Task Algo

--- RLMatrix.py
+++ RLMatrix.py
@@ -6,11 +6,11 @@ sns.set_style("darkgrid")
 import random
 
 gamma = 1 # discounting rate
-rewardSize = -0.25
+rewardSize = -1
 gridSize = 5
 terminationStates = [[0,0], [gridSize-1, gridSize-1]]
 actions = [[-1, 0], [1, 0], [0, 1], [0, -1]]
-numIterations = 1
+numIterations = 1000
 
 def actionRewardFunction(initialPosition, action):
     
@@ -19,8 +19,8 @@ def actionRewardFunction(initialPosition, action):
     
     reward = rewardSize
     #print("reward",reward)
-    print("initial",initialPosition)
-    print("action",action)
+    #print("initial",initialPosition)
+    #print("action",action)
     finalPosition = np.array(initialPosition) + np.array(action)
     #print("finalPosition",finalPosition)
     if -1 in finalPosition or 5 in finalPosition: 
@@ -30,37 +30,28 @@ def actionRewardFunction(initialPosition, action):
     return finalPosition, reward
 
 valueMap = np.zeros((gridSize, gridSize))
-print("valuemap",range(gridSize))
+#print("valuemap",range(gridSize))
 states = [[i, j] for i in range(gridSize) for j in range(gridSize)]
-print("'States",states)
-valueMap
+#print("'States",states)
 deltas = []
 for it in range(numIterations):
     copyValueMap = np.copy(valueMap)
+    #print("valueMap",copyValueMap)
     deltaState = []
     for state in states:
-        print("--------------new State--------\n",state)
+        #print("--------------new State--------\n",state)
         weightedRewards = 0
-        actions = [[-1, 0], [1, 0], [0, 1], [0, -1]]
+        statevalue =0
         for action in actions:
-            print("action",action)
+            #print("action",action)
             #print("length",(1/len(actions)))
             finalPosition, reward = actionRewardFunction(state, action)
-            print("reward",reward)
-            #print("finalPosition[0]",finalPosition[0])
-            #print("finalPosition[1]",finalPosition[1])
-            print("valueMap[finalPosition[0], finalPosition[1]",valueMap[finalPosition[0], finalPosition[1]])
-
-            weightedRewards += ((1/len(actions))*(reward+(gamma*valueMap[finalPosition[0], finalPosition[1]])))
-            print("weightedvalue",weightedRewards)
-            print("copyValueMap[state[0], state[1]]",copyValueMap[state[0], state[1]])
-            #deltaState.append(np.abs(copyValueMap[state[0], state[1]]-weightedRewards))
-            #deltaState.append(np.abs(-1 + weightedRewards))
-           #print("deltaState",deltaState)
-            copyValueMap[state[0], state[1]] = -1+ weightedRewards
-            #deltas.append(deltaState)
-            valueMap = copyValueMap
+            weightedRewards += (1/len(actions))*(reward+(gamma*valueMap[finalPosition[0], finalPosition[1]]))
+        deltaState.append(np.abs(copyValueMap[state[0], state[1]]-weightedRewards))
+        copyValueMap[state[0], state[1]] = weightedRewards 
+    deltas.append(deltaState)
+    valueMap = copyValueMap
     if it in [0,1,2,9, 99, numIterations-1]:
-        print("Iteration {}".format(it))
+        print("Iteration {}".format(it+1))
         print(valueMap)
         print("")
